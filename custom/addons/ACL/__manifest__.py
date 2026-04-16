49a88c89ee1fec18a20360b0084cad428999870f
Vidhya <ebsvidyapl@gmail.com>
2026-04-16 15:18:21 +0530

    Add po button in so

--- custom/addons/ACL/__manifest__.py
+++ custom/addons/ACL/__manifest__.py
@@ -1,10 +1,13 @@
 {
     'name': 'ACL Role Management',
     'version': '19.0.1',
-    'depends': ['base', 'sale', 'stock', 'account'],
+    'depends': ['base', 'sale', 'stock', 'account','purchase' , 'crm','sale_management', 'stock'],
     'data': [
         'security/security.xml',
         'security/ir.model.access.csv',
+
+        'views/sale_order_views.xml', 
     ],
     'installable': True,
+    'application': True,
 }
