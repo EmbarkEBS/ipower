91722d9894e3c05ed9a54baf750d1966ac76fd97
Vidhya <ebsvidyapl@gmail.com>
2026-04-18 17:06:59 +0530

    Force commit

--- custom/addons/ACL/__manifest__.py
+++ custom/addons/ACL/__manifest__.py
@@ -3,7 +3,7 @@
     'version': '19.0.1',
     'depends': ['base', 'sale', 'stock', 'account','purchase' , 'crm','sale_management','mail'],
     'data': [
-        'security/security.xml',
+        #'security/security.xml',
         'security/ir.model.access.csv',
 
         'views/sale_order_views.xml', 
