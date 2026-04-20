707ede2f37b1ef6aabb8bd3ef8536541a4a382e4
Vidhya <ebsvidyapl@gmail.com>
2026-04-20 22:24:42 +0530

    force commit

--- custom/addons/ACL/__manifest__.py
+++ custom/addons/ACL/__manifest__.py
@@ -3,8 +3,12 @@
     'version': '19.0.1',
     'depends': ['base', 'sale', 'stock', 'account','purchase', 'crm','sale_management', 'stock'],
     'data': [
+<<<<<<< HEAD
 
         'security/security.xml',
+=======
+       # 'security/security.xml',
+>>>>>>> parent of 3936c5e (bug fix add po to so 1)
         'security/ir.model.access.csv',
 
 
