a0d94e26500b255a59aab1de6d4505b90d286956
Vidhya <ebsvidyapl@gmail.com>
2026-04-20 21:58:39 +0530

    add po restriction

--- custom/addons/ACL/__manifest__.py
+++ custom/addons/ACL/__manifest__.py
@@ -3,13 +3,10 @@
     'version': '19.0.1',
     'depends': ['base', 'sale', 'stock', 'account','purchase' , 'crm','sale_management', 'stock'],
     'data': [
-<<<<<<< HEAD
+
        # 'security/security.xml',
         #'security/ir.model.access.csv',
-=======
-        'security/security.xml',
-        'security/ir.model.access.csv',
->>>>>>> 3936c5e7f990485dd18d153535bd814082995d25
+
 
         'views/sale_order_views.xml', 
     ],
