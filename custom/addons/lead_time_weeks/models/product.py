ec697c4061d310c659026f227a6d8e0ed8c04f98
Vidhya <ebsvidyapl@gmail.com>
2026-04-22 13:41:09 +0530

    Add module lead time weeks invetory product form 2

--- custom/addons/lead_time_weeks/models/product.py
+++ custom/addons/lead_time_weeks/models/product.py
@@ -1,20 +1,20 @@
 from odoo import models, fields, api
 
-class ProductTemplate(models.Model):
-    _inherit = 'product.template'
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
 
     lead_time_weeks = fields.Float(
         string="Customer Lead Time (Weeks)",
-        compute="_compute_weeks",
-        inverse="_inverse_weeks",
+        compute="_compute_lead_time_weeks",
+        inverse="_inverse_lead_time_weeks",
         store=True
     )
 
     @api.depends('sale_delay')
-    def _compute_weeks(self):
+    def _compute_lead_time_weeks(self):
         for rec in self:
-            rec.lead_time_weeks = rec.sale_delay / 7 if rec.sale_delay else 0.0
+            rec.lead_time_weeks = rec.sale_delay / 7.0 if rec.sale_delay else 0.0
 
-    def _inverse_weeks(self):
+    def _inverse_lead_time_weeks(self):
         for rec in self:
-            rec.sale_delay = rec.lead_time_weeks * 7 if rec.lead_time_weeks else 0.0
+            rec.sale_delay = rec.lead_time_weeks * 7.0 if rec.lead_time_weeks else 0.0
