7499232a9e0598d61cacbfd2e0a046af9a128c0e
Vidhya <ebsvidyapl@gmail.com>
2026-04-22 14:12:33 +0530

    Add module lead time weeks invetory product form 7

--- custom/addons/lead_time_weeks/models/product.py
+++ custom/addons/lead_time_weeks/models/product.py
@@ -5,17 +5,17 @@ class ProductTemplate(models.Model):
 
     lead_time_weeks = fields.Float(
         string="Customer Lead Time (Weeks)",
-        compute="_compute_lead_time_weeks",
-        inverse="_inverse_lead_time_weeks",
+        compute="_compute_weeks",
+        inverse="_inverse_weeks",
         store=True
     )
 
-    def _compute_lead_time_weeks(self):
+    def _compute_weeks(self):
         for rec in self:
             sale_delay = getattr(rec, 'sale_delay', 0.0)
             rec.lead_time_weeks = sale_delay / 7.0 if sale_delay else 0.0
 
-    def _inverse_lead_time_weeks(self):
+    def _inverse_weeks(self):
         for rec in self:
             if hasattr(rec, 'sale_delay'):
                 rec.sale_delay = rec.lead_time_weeks * 7.0
