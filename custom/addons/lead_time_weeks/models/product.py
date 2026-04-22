f3de95ef72667c6c93569f6dbc73ef401d4b39fb
Vidhya <ebsvidyapl@gmail.com>
2026-04-22 14:33:42 +0530

    Add module lead time weeks invetory product form 10

--- custom/addons/lead_time_weeks/models/product.py
+++ custom/addons/lead_time_weeks/models/product.py
@@ -3,7 +3,7 @@ from odoo import models, fields
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    lead_time_weeks = fields.Integer(string="Customer Lead Time (Weeks)")
+    lead_time_weeks = fields.Integer(string="Lead Time (Weeks)")
 
     def write(self, vals):
         if 'lead_time_weeks' in vals:
