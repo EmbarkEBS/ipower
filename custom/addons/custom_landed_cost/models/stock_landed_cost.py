7bc6f951c0603c7a8628bb30551f92f2d4402106
Vidhya <ebsvidyapl@gmail.com>
2026-04-27 13:05:43 +0530

    Added Dinesh Landed cost hs code module-3

--- custom/addons/custom_landed_cost/models/stock_landed_cost.py
+++ custom/addons/custom_landed_cost/models/stock_landed_cost.py
@@ -3,21 +3,18 @@ from odoo import models
 class StockLandedCost(models.Model):
     _inherit = "stock.landed.cost"
 
-    def _get_valuation_lines(self):
-        valuation_lines = super()._get_valuation_lines()
-
+    def button_validate(self):
         for cost in self:
-            for line in cost.cost_lines:
 
-                # Only Custom Duty line (better: use boolean field instead of name)
-                if line.product_id.name == "Custom Duty":
+            for val in cost.valuation_adjustment_lines:
 
-                    exemption = line.product_id.product_tmpl_id.x_studio_related_field_8pf_1jn6mtke2
+                product = val.product_id
+                if product and product.product_tmpl_id:
 
-                    # CASE 0% → remove landed cost impact
+                    exemption = product.product_tmpl_id.x_studio_related_field_8pf_1jn6mtke2
+
+                    # 0% → remove landed cost impact
                     if exemption == "0%":
-                        for val in valuation_lines:
-                            if val.cost_line_id == line:
-                                val.additional_landed_cost = 0
+                        val.additional_landed_cost = 0
 
-        return valuation_lines
+        return super().button_validate()
