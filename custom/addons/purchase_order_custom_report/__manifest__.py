ec15cf19133f0fad03d55426084861e67ee0a1e9
Vidhya <ebsvidyapl@gmail.com>
2026-04-27 15:48:44 +0530

    po s report

--- custom/addons/purchase_order_custom_report/__manifest__.py
+++ custom/addons/purchase_order_custom_report/__manifest__.py
@@ -1,10 +1,12 @@
 {
-    'name': 'Custom Purchase Order Report',
+    'name': 'Custom  Report',
     'version': '1.0',
     'depends': ['purchase'],
     'data': [
         'reports/purchase_order_report.xml',
         'reports/purchase_order_template.xml',
+        'reports/sale_order_report.xml',
+        'reports/sale_order_template.xml',
     ],
     'installable': True,
 }
