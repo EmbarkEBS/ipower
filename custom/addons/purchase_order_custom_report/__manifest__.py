d7a78893dc1863fc9481dc394b724a7464376450
Vidhya <ebsvidyapl@gmail.com>
2026-04-28 21:16:43 +0530

    force commit

---
+++ custom/addons/purchase_order_custom_report/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Custom  Report',
+    'version': '19.0',
+    'depends': ['purchase'],
+    'data': [
+        'reports/purchase_order_report.xml',
+        'reports/purchase_order_template.xml',
+        'reports/sale_order_report.xml',
+        'reports/sale_order_template.xml',
+    ],
+    'installable': True,
+}
