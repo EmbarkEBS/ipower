b3bdf65da441de37c33b9a565c1dba4386da5b67
Vidhya <ebsvidyapl@gmail.com>
2026-05-02 22:16:51 +0530

    add editable option 25

--- custom/addons/sale_internal_cost/models/sale_order.py
+++ custom/addons/sale_internal_cost/models/sale_order.py
@@ -13,29 +13,33 @@ class SaleOrder(models.Model):
         for order in self:
             order.total_extra = (order.freight or 0.0) + (order.duty or 0.0) + (order.misc or 0.0)
 
-    @api.onchange('freight', 'duty', 'misc', 'order_line.product_uom_qty')
-    def _onchange_distribute_charges(self):
-        """Calculates the share of extra cost for every line without touching price_unit."""
+    @api.onchange('freight', 'duty', 'misc')
+    def _onchange_extra_costs(self):
+        self._recalculate_line_prices()
+
+    def _recalculate_line_prices(self):
         for order in self:
             lines = order.order_line.filtered(lambda l: not l.display_type)
+            if not lines:
+                continue
             total_qty = sum(lines.mapped('product_uom_qty')) or 1.0
             extra_per_unit = order.total_extra / total_qty
             for line in lines:
-                line.internal_charge = extra_per_unit
+                # Priority: Manual Price > Product Price
+                base = line.manual_base_price if line.manual_base_price > 0 else line.product_id.lst_price
+                line.price_unit = base + extra_per_unit
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    # This field shows the distributed cost share
-    internal_charge = fields.Float(string="Int. Charge", readonly=True)
-    
-    # We create a new subtotal that includes the internal charge for your reference
-    price_with_charge = fields.Float(
-        string="Total Unit Cost", 
-        compute="_compute_price_with_charge"
-    )
+    manual_base_price = fields.Float(string="Base Price", digits='Product Price')
 
-    @api.depends('price_unit', 'internal_charge')
-    def _compute_price_with_charge(self):
-        for line in self:
-            line.price_with_charge = line.price_unit + line.internal_charge
+    @api.onchange('product_id')
+    def _onchange_product_id_set_base(self):
+        if self.product_id:
+            self.manual_base_price = self.product_id.lst_price
+
+    @api.onchange('manual_base_price', 'product_uom_qty')
+    def _onchange_recompute_final_price(self):
+        if self.order_id:
+            self.order_id._recalculate_line_prices()
