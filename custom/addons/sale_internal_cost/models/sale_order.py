f8e7122c8a6a67d85fb7033efec7ac5c20d4cb69
Vidhya <ebsvidyapl@gmail.com>
2026-05-02 22:19:28 +0530

    add editable option 26

--- custom/addons/sale_internal_cost/models/sale_order.py
+++ custom/addons/sale_internal_cost/models/sale_order.py
@@ -13,11 +13,9 @@ class SaleOrder(models.Model):
         for order in self:
             order.total_extra = (order.freight or 0.0) + (order.duty or 0.0) + (order.misc or 0.0)
 
-    @api.onchange('freight', 'duty', 'misc')
-    def _onchange_extra_costs(self):
-        self._recalculate_line_prices()
-
-    def _recalculate_line_prices(self):
+    @api.onchange('freight', 'duty', 'misc', 'order_line.product_uom_qty')
+    def _onchange_distribute_charges(self):
+        """Calculates the charge share per line without overwriting the price."""
         for order in self:
             lines = order.order_line.filtered(lambda l: not l.display_type)
             if not lines:
@@ -25,21 +23,22 @@ class SaleOrder(models.Model):
             total_qty = sum(lines.mapped('product_uom_qty')) or 1.0
             extra_per_unit = order.total_extra / total_qty
             for line in lines:
-                # Priority: Manual Price > Product Price
-                base = line.manual_base_price if line.manual_base_price > 0 else line.product_id.lst_price
-                line.price_unit = base + extra_per_unit
+                line.internal_charge = extra_per_unit
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    manual_base_price = fields.Float(string="Base Price", digits='Product Price')
+    # This field holds only the extra cost part
+    internal_charge = fields.Float(string="Internal Charge", readonly=True)
+    
+    # We override the price_subtotal to include our charge
+    price_subtotal = fields.Monetary(compute='_compute_amount', string='Subtotal', store=True)
 
-    @api.onchange('product_id')
-    def _onchange_product_id_set_base(self):
-        if self.product_id:
-            self.manual_base_price = self.product_id.lst_price
-
-    @api.onchange('manual_base_price', 'product_uom_qty')
-    def _onchange_recompute_final_price(self):
-        if self.order_id:
-            self.order_id._recalculate_line_prices()
+    @api.depends('product_uom_qty', 'discount', 'price_unit', 'tax_id', 'internal_charge')
+    def _compute_amount(self):
+        """Override standard subtotal to include the internal charge."""
+        super(SaleOrderLine, self)._compute_amount()
+        for line in self:
+            # Add (Charge * Qty) to the standard subtotal
+            charge_amount = line.internal_charge * line.product_uom_qty
+            line.price_subtotal += charge_amount
