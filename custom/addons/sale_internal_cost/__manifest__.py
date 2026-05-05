09476d2b22396980b08ec3673f887fa10d630ee1
Vidhya <ebsvidyapl@gmail.com>
2026-05-05 16:30:16 +0530

    ic with 0 tax s4

--- custom/addons/sale_internal_cost/__manifest__.py
+++ custom/addons/sale_internal_cost/__manifest__.py
@@ -2,11 +2,14 @@
 {
     'name': 'Sale Internal Cost',
     'version': '1.0',
-    'depends': ['sale'],
+    'depends': ['sale', 'account'],
     'author': 'vidhya',
     'category': 'Sales',
     'summary': 'Add Freight, Duty, Misc to Sale Order and include in price',
-    'data': ['views/sale_order_views.xml','views/report_saleorder.xml'],
+    'data': [
+        'views/sale_order_views.xml',
+        'views/report_saleorder.xml',
+    ],
     'installable': True,
     'application': False,
 }
--- custom/addons/sale_internal_cost/models/sale_order.py
+++ custom/addons/sale_internal_cost/models/sale_order.py
@@ -9,39 +9,31 @@ class SaleOrder(models.Model):
 
     @api.onchange('freight', 'duty', 'misc')
     def _onchange_update_charge_line(self):
-        """Adds, updates, or REMOVES the internal charge line."""
+        """Automatically manages the internal charge line."""
         for order in self:
             total_charge = (order.freight or 0.0) + (order.duty or 0.0) + (order.misc or 0.0)
             
-            # Find the special charge line using the boolean flag
+            # Find existing charge line using the boolean flag
             charge_line = order.order_line.filtered(lambda l: l.x_is_charge_line)
             
             if total_charge > 0:
                 if charge_line:
-                    # Update price on the existing line
+                    # Update price if line exists
                     charge_line[0].price_unit = total_charge
                 else:
-                    # Create the new line
+                    # Create new line if it doesn't exist
                     new_line_vals = {
                         'name': 'Internal Charges (Freight, Duty, Misc)',
                         'product_uom_qty': 1.0,
                         'price_unit': total_charge,
-                        'tax_ids': [(6, 0, [])], # No taxes
+                        'tax_ids': [(6, 0, [])],  # No taxes applied
                         'x_is_charge_line': True,
                     }
-                    # Command (0, 0, vals) adds the line
                     order.order_line = [(0, 0, new_line_vals)]
             else:
-                # IF CHARGE IS 0: Remove the line from the UI
+                # If charges become 0, remove the line
                 if charge_line:
-                    # We use command (2, ID, 0) to delete the line from the view
-                    # We loop in case there are multiple by mistake
-                    commands = []
-                    for line in charge_line:
-                        # If the line exists in DB, use ID. If it's new (Virtual), use _origin
-                        line_id = line.id.origin if hasattr(line.id, 'origin') else line.id
-                        commands.append((2, line_id, 0))
-                    order.order_line = commands
+                    order.order_line -= charge_line
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
