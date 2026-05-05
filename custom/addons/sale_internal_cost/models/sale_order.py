6a837ca5a0b4b2b4b59086716986ce4ef777e1b4
Vidhya <ebsvidyapl@gmail.com>
2026-05-05 15:00:12 +0530

    tree to list 2

--- custom/addons/sale_internal_cost/models/sale_order.py
+++ custom/addons/sale_internal_cost/models/sale_order.py
@@ -38,6 +38,7 @@ class SaleOrder(models.Model):
 
             for line in lines:
                 line.x_internal_charge = extra_per_unit
+                line._update_price_unit()
 
 
 # =========================
@@ -61,51 +62,27 @@ class SaleOrderLine(models.Model):
     # -------------------------
     @api.onchange('product_id')
     def _onchange_product_id_set_base(self):
-        if self.product_id:
-            self.x_base_price = self.product_id.lst_price
-            self.price_unit = self.x_base_price  # IMPORTANT
-
-
-    # -------------------------
-    # Keep price_unit = base
-    # -------------------------
-    @api.onchange('x_base_price')
-    def _onchange_base_price(self):
         for line in self:
-            line.price_unit = line.x_base_price
-
+            if line.product_id:
+                line.x_base_price = line.product_id.lst_price
+                line._update_price_unit()
 
     # -------------------------
-    # CUSTOM AMOUNT COMPUTATION
+    # Update final unit price
     # -------------------------
-    @api.depends(
-        'product_uom_qty',
-        'discount',
-        'price_unit',
-        'tax_ids',
-        'x_internal_charge'
-    )
-    def _compute_amount(self):
+    @api.onchange('x_base_price', 'x_internal_charge')
+    def _update_price_unit(self):
         for line in self:
-            # Base price after discount
-            base_price = line.price_unit * (1 - (line.discount or 0.0) / 100.0)
+            line.price_unit = (line.x_base_price or 0.0) + (line.x_internal_charge or 0.0)
 
-            # Internal charge added ONLY to subtotal
-            total_unit_price = base_price + (line.x_internal_charge or 0.0)
+    # -------------------------
+    # TAX FIX (IMPORTANT)
+    # -------------------------
+    def _prepare_base_line_for_taxes_computation(self):
+        res = super()._prepare_base_line_for_taxes_computation()
 
-            # TAX computed ONLY on base price
-            taxes = line.tax_ids.compute_all(
-                base_price,
-                currency=line.order_id.currency_id,
-                quantity=line.product_uom_qty,
-                product=line.product_id,
-                partner=line.order_id.partner_shipping_id,
-            )
+        # Force tax engine to use ONLY base price
+        if self.x_base_price:
+            res['price_unit'] = self.x_base_price
 
-            tax_amount = taxes['total_included'] - taxes['total_excluded']
-
-            line.update({
-                'price_subtotal': total_unit_price * line.product_uom_qty,
-                'price_tax': tax_amount,
-                'price_total': (total_unit_price * line.product_uom_qty) + tax_amount,
-            })
+        return res
