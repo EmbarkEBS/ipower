949d0dba7622d98577a63d13d9c75850ab1fa3bb
Vidhya <ebsvidyapl@gmail.com>
2026-05-05 15:54:00 +0530

    ic with 0 tax

--- custom/addons/sale_internal_cost/models/sale_order.py
+++ custom/addons/sale_internal_cost/models/sale_order.py
@@ -13,70 +13,28 @@ class SaleOrder(models.Model):
         for order in self:
             total_charge = (order.freight or 0.0) + (order.duty or 0.0) + (order.misc or 0.0)
             
-            # Find if our custom 'Internal Charge' line already exists
+            # Find the custom charge line
             charge_line = order.order_line.filtered(lambda l: l.x_is_charge_line)
             
             if total_charge > 0:
                 if charge_line:
-                    # Update the existing line price
+                    # Update price on existing line
                     charge_line.price_unit = total_charge
                 else:
-                    # Create a new line. Note: using 'tax_id' as required by your server
-                    order.order_line = [(0, 0, {
+                    # Create new line. NOTE: Using 'tax_id' (plural) as required by Odoo 19
+                    new_line_vals = {
                         'name': 'Internal Charges (Freight, Duty, Misc)',
                         'product_uom_qty': 1.0,
                         'price_unit': total_charge,
-                        'tax_id': [(6, 0, [])], # No taxes on this charge
+                        'tax_id': [(6, 0, [])], # This ensures the charge is NOT taxed
                         'x_is_charge_line': True,
-                    })]
+                    }
+                    order.order_line = [(0, 0, new_line_vals)]
             elif charge_line:
-                # Remove the line if all charges are set to zero
+                # If charges are cleared, remove the line
                 order.order_line = [(2, charge_line.id, 0)]
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    # Hidden field to identify the special charge line so we don't delete the wrong line
-    x_is_charge_line = fields.Boolean(string="Is Charge Line", default=False)
-from odoo import models, fields, api
-
-class SaleOrder(models.Model):
-    _inherit = 'sale.order'
-
-    freight = fields.Monetary(string="Freight")
-    duty = fields.Monetary(string="Duty")
-    misc = fields.Monetary(string="Miscellaneous")
-
-    @api.onchange('freight', 'duty', 'misc')
-    def _onchange_update_charge_line(self):
-        """Adds or updates a separate line for all internal charges."""
-        for order in self:
-            total_charge = (order.freight or 0.0) + (order.duty or 0.0) + (order.misc or 0.0)
-            
-            # Find if our custom 'Internal Charge' line already exists
-            charge_line = order.order_line.filtered(lambda l: l.x_is_charge_line)
-            
-            if total_charge > 0:
-                if charge_line:
-                    # Update existing line
-                    charge_line[0].price_unit = total_charge
-                else:
-                    # Create new line at the bottom. FIX: Use 'tax_id' (plural)
-                    # Use a list of commands (0, 0, values) to add the line
-                    new_line_vals = {
-                        'name': 'Internal Charges (Freight, Duty, Misc)',
-                        'product_uom_qty': 1.0,
-                        'price_unit': total_charge,
-                        'tax_id': [(6, 0, [])], # No taxes applied to this line
-                        'x_is_charge_line': True,
-                    }
-                    order.order_line = [(0, 0, new_line_vals)]
-            elif charge_line:
-                # Remove the line if all charges are cleared
-                order.order_line = [(2, charge_line[0].id, 0)]
-
-class SaleOrderLine(models.Model):
-    _inherit = 'sale.order.line'
-
-    # Hidden field to identify the special charge line
     x_is_charge_line = fields.Boolean(string="Is Charge Line", default=False)
