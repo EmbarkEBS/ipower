ccdf5eabbc19907752df90811702e07d3282eca3
Vidhya <ebsvidyapl@gmail.com>
2026-05-05 16:38:48 +0530

    ic with 0 tax s6

--- custom/addons/sale_internal_cost/models/sale_order.py
+++ custom/addons/sale_internal_cost/models/sale_order.py
@@ -13,44 +13,38 @@ class SaleOrder(models.Model):
         for order in self:
             order.total_extra = (order.freight or 0.0) + (order.duty or 0.0) + (order.misc or 0.0)
 
-    @api.onchange('freight', 'duty', 'misc')
+    @api.onchange('freight', 'duty', 'misc', 'total_extra')
     def _onchange_extra_costs(self):
         self._recalculate_line_prices()
 
     def _recalculate_line_prices(self):
         for order in self:
+            # Filter for real product lines only
             lines = order.order_line.filtered(lambda l: not l.display_type)
             if not lines:
                 continue
+            
             total_qty = sum(lines.mapped('product_uom_qty')) or 1.0
             extra_per_unit = order.total_extra / total_qty
+            
             for line in lines:
+                # Use x_base_price as the starting point for math
                 base = line.x_base_price if line.x_base_price > 0 else line.product_id.lst_price
                 line.price_unit = base + extra_per_unit
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
+    # Base price used for internal calculation
     x_base_price = fields.Float(string="Base Price", digits='Product Price')
 
     @api.onchange('product_id')
     def _onchange_product_id_set_base(self):
         if self.product_id:
+            # Set base price from product's standard list price
             self.x_base_price = self.product_id.lst_price
 
     @api.onchange('x_base_price', 'product_uom_qty')
     def _onchange_recompute_final_price(self):
         if self.order_id:
             self.order_id._recalculate_line_prices()
-
-    def _prepare_base_line_for_taxes_computation(self):
-        """
-        Odoo 17/18/19 Hook: Intercepts the tax engine.
-        Forces the tax engine to use x_base_price for its math,
-        ignoring the internal charges added to price_unit.
-        """
-        res = super()._prepare_base_line_for_taxes_computation()
-        if self.x_base_price > 0:
-            # We tell the tax engine that the price for TAX math is x_base_price
-            res['price_unit'] = self.x_base_price
-        return res
