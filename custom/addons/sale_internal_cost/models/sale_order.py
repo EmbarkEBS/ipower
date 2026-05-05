173fba2a9378fa244a8fe317a01b724573faa478
Vidhya <ebsvidyapl@gmail.com>
2026-05-05 16:03:12 +0530

    ic with 0 tax s

--- custom/addons/sale_internal_cost/models/sale_order.py
+++ custom/addons/sale_internal_cost/models/sale_order.py
@@ -13,7 +13,7 @@ class SaleOrder(models.Model):
         for order in self:
             total_charge = (order.freight or 0.0) + (order.duty or 0.0) + (order.misc or 0.0)
             
-            # Find the custom charge line
+            # Find our custom 'Internal Charge' line
             charge_line = order.order_line.filtered(lambda l: l.x_is_charge_line)
             
             if total_charge > 0:
@@ -21,20 +21,22 @@ class SaleOrder(models.Model):
                     # Update price on existing line
                     charge_line.price_unit = total_charge
                 else:
-                    # Create new line. NOTE: Using 'tax_id' (plural) as required by Odoo 19
+                    # Create new line at the bottom. 
+                    # FIX: Use 'tax_id' as correctly identified.
                     new_line_vals = {
                         'name': 'Internal Charges (Freight, Duty, Misc)',
                         'product_uom_qty': 1.0,
                         'price_unit': total_charge,
-                        'tax_id': [(6, 0, [])], # This ensures the charge is NOT taxed
+                        'tax_id': [(6, 0, [])], # No taxes applied
                         'x_is_charge_line': True,
                     }
                     order.order_line = [(0, 0, new_line_vals)]
             elif charge_line:
-                # If charges are cleared, remove the line
+                # Remove the line if all charges are set to zero
                 order.order_line = [(2, charge_line.id, 0)]
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
+    # Hidden field to identify our custom charge line
     x_is_charge_line = fields.Boolean(string="Is Charge Line", default=False)
