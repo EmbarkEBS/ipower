d1e8a2bc2c035d974103c791e0daccd27536cad6
Vidhya <ebsvidyapl@gmail.com>
2026-05-05 16:07:00 +0530

    ic with 0 tax s1

--- custom/addons/sale_internal_cost/models/sale_order.py
+++ custom/addons/sale_internal_cost/models/sale_order.py
@@ -13,7 +13,7 @@ class SaleOrder(models.Model):
         for order in self:
             total_charge = (order.freight or 0.0) + (order.duty or 0.0) + (order.misc or 0.0)
             
-            # Find our custom 'Internal Charge' line
+            # Find our custom 'Internal Charge' line using the boolean flag
             charge_line = order.order_line.filtered(lambda l: l.x_is_charge_line)
             
             if total_charge > 0:
@@ -21,8 +21,7 @@ class SaleOrder(models.Model):
                     # Update price on existing line
                     charge_line.price_unit = total_charge
                 else:
-                    # Create new line at the bottom. 
-                    # FIX: Use 'tax_id' as correctly identified.
+                    # Create new line. NOTE: Using 'tax_id' (plural) as identified
                     new_line_vals = {
                         'name': 'Internal Charges (Freight, Duty, Misc)',
                         'product_uom_qty': 1.0,
@@ -30,13 +29,14 @@ class SaleOrder(models.Model):
                         'tax_id': [(6, 0, [])], # No taxes applied
                         'x_is_charge_line': True,
                     }
+                    # We add the line using the command list format
                     order.order_line = [(0, 0, new_line_vals)]
             elif charge_line:
-                # Remove the line if all charges are set to zero
+                # Remove the charge line if total becomes zero
                 order.order_line = [(2, charge_line.id, 0)]
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    # Hidden field to identify our custom charge line
+    # Boolean to identify the special charge line so we don't affect product lines
     x_is_charge_line = fields.Boolean(string="Is Charge Line", default=False)
