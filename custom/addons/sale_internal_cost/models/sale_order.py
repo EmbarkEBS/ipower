a4719cc55ca47af80db24d6071f272edc45a6331
Vidhya <ebsvidyapl@gmail.com>
2026-05-05 14:40:18 +0530

    Sale Internal Cost completecode change2

--- custom/addons/sale_internal_cost/models/sale_order.py
+++ custom/addons/sale_internal_cost/models/sale_order.py
@@ -15,7 +15,7 @@ class SaleOrder(models.Model):
 
     @api.onchange('freight', 'duty', 'misc', 'order_line.product_uom_qty')
     def _onchange_distribute_costs(self):
-        """Calculates the charge share for each line."""
+        """Spreads global costs and forces lines to update their total price."""
         for order in self:
             lines = order.order_line.filtered(lambda l: not l.display_type)
             if not lines:
@@ -24,32 +24,33 @@ class SaleOrder(models.Model):
             extra_per_unit = order.total_extra / total_qty
             for line in lines:
                 line.x_internal_charge = extra_per_unit
+                # Force the update of the 'Price + Charges' and 'Amount'
+                line._update_price_unit_total()
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
     x_base_price = fields.Float(string="Unit Price", digits='Product Price')
-    x_internal_charge = fields.Float(string="Charge Share", readonly=True)
+    x_internal_charge = fields.Float(string="Int. Charge", readonly=True)
 
     @api.onchange('product_id')
     def _onchange_product_id_set_base(self):
         if self.product_id:
             self.x_base_price = self.product_id.lst_price
 
-    @api.onchange('x_base_price', 'x_internal_charge')
-    def _onchange_update_price_unit(self):
-        """
-        This is the key: price_unit becomes the Total (Base + Charge).
-        Odoo will then automatically calculate the 'Amount' from this.
-        """
-        self.price_unit = self.x_base_price + self.x_internal_charge
+    @api.onchange('x_base_price', 'x_internal_charge', 'product_uom_qty')
+    def _update_price_unit_total(self):
+        """Sets the official Price Unit to include charges so the Subtotal (Amount) is correct."""
+        for line in self:
+            line.price_unit = line.x_base_price + line.x_internal_charge
 
     def _prepare_base_line_for_taxes_computation(self):
         """
-        THE TAX FIX: We tell the tax engine to use x_base_price 
-        for math, so internal charges are NOT taxed.
+        THE TAX FIX: Forces the tax engine to use Unit Price (1720) 
+        for math, ignoring the charges added to price_unit.
         """
         res = super()._prepare_base_line_for_taxes_computation()
+        # If we have a base price, use it for taxes. Otherwise use the default.
         if self.x_base_price > 0:
             res['price_unit'] = self.x_base_price
         return res
