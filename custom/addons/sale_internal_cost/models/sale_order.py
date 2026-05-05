10612b44580a5558450dfb2f971591a4f9ca5b50
Vidhya <ebsvidyapl@gmail.com>
2026-05-05 15:47:23 +0530

    chane order line  tkt bug fix1 revert code

--- custom/addons/sale_internal_cost/models/sale_order.py
+++ custom/addons/sale_internal_cost/models/sale_order.py
@@ -11,30 +11,30 @@ class SaleOrder(models.Model):
     def _onchange_update_charge_line(self):
         """Adds or updates a separate line for all internal charges."""
         for order in self:
-            total_charge = (order.freight or 0) + (order.duty or 0) + (order.misc or 0)
+            total_charge = (order.freight or 0.0) + (order.duty or 0.0) + (order.misc or 0.0)
             
-            # Find if the 'Internal Charge' line already exists
+            # Find if our custom 'Internal Charge' line already exists
             charge_line = order.order_line.filtered(lambda l: l.x_is_charge_line)
             
             if total_charge > 0:
                 if charge_line:
-                    # Update existing line
-                    charge_line[0].price_unit = total_charge
+                    # Update the existing line price
+                    charge_line.price_unit = total_charge
                 else:
-                    # Create new line at the bottom with NO TAX
+                    # Create a new line. Note: using 'tax_id' as required by your server
                     order.order_line = [(0, 0, {
                         'name': 'Internal Charges (Freight, Duty, Misc)',
-                        'product_uom_qty': 1,
+                        'product_uom_qty': 1.0,
                         'price_unit': total_charge,
-                        'tax_id': [(6, 0, [])], # No taxes applied
+                        'tax_id': [(6, 0, [])], # No taxes on this charge
                         'x_is_charge_line': True,
                     })]
             elif charge_line:
-                # Remove line if charges are set to zero
-                order.order_line = [(2, charge_line[0].id, 0)]
+                # Remove the line if all charges are set to zero
+                order.order_line = [(2, charge_line.id, 0)]
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    # Hidden field to identify our custom charge line
+    # Hidden field to identify the special charge line so we don't delete the wrong line
     x_is_charge_line = fields.Boolean(string="Is Charge Line", default=False)
