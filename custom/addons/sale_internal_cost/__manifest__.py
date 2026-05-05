73cc38a4cc64558914395fd8cbd7598f61a936d9
Vidhya <ebsvidyapl@gmail.com>
2026-05-05 16:35:17 +0530

    ic with 0 tax s5

--- custom/addons/sale_internal_cost/__manifest__.py
+++ custom/addons/sale_internal_cost/__manifest__.py
@@ -2,14 +2,11 @@
 {
     'name': 'Sale Internal Cost',
     'version': '1.0',
-    'depends': ['sale', 'account'],
+    'depends': ['sale'],
     'author': 'vidhya',
     'category': 'Sales',
     'summary': 'Add Freight, Duty, Misc to Sale Order and include in price',
-    'data': [
-        'views/sale_order_views.xml',
-        'views/report_saleorder.xml',
-    ],
+    'data': ['views/sale_order_views.xml'],
     'installable': True,
     'application': False,
 }
--- custom/addons/sale_internal_cost/models/sale_order.py
+++ custom/addons/sale_internal_cost/models/sale_order.py
@@ -6,36 +6,51 @@ class SaleOrder(models.Model):
     freight = fields.Monetary(string="Freight")
     duty = fields.Monetary(string="Duty")
     misc = fields.Monetary(string="Miscellaneous")
+    total_extra = fields.Monetary(string="Total Extra", compute="_compute_total_extra", store=True)
+
+    @api.depends('freight', 'duty', 'misc')
+    def _compute_total_extra(self):
+        for order in self:
+            order.total_extra = (order.freight or 0.0) + (order.duty or 0.0) + (order.misc or 0.0)
 
     @api.onchange('freight', 'duty', 'misc')
-    def _onchange_update_charge_line(self):
-        """Automatically manages the internal charge line."""
+    def _onchange_extra_costs(self):
+        self._recalculate_line_prices()
+
+    def _recalculate_line_prices(self):
         for order in self:
-            total_charge = (order.freight or 0.0) + (order.duty or 0.0) + (order.misc or 0.0)
-            
-            # Find existing charge line using the boolean flag
-            charge_line = order.order_line.filtered(lambda l: l.x_is_charge_line)
-            
-            if total_charge > 0:
-                if charge_line:
-                    # Update price if line exists
-                    charge_line[0].price_unit = total_charge
-                else:
-                    # Create new line if it doesn't exist
-                    new_line_vals = {
-                        'name': 'Internal Charges (Freight, Duty, Misc)',
-                        'product_uom_qty': 1.0,
-                        'price_unit': total_charge,
-                        'tax_ids': [(6, 0, [])],  # No taxes applied
-                        'x_is_charge_line': True,
-                    }
-                    order.order_line = [(0, 0, new_line_vals)]
-            else:
-                # If charges become 0, remove the line
-                if charge_line:
-                    order.order_line -= charge_line
+            lines = order.order_line.filtered(lambda l: not l.display_type)
+            if not lines:
+                continue
+            total_qty = sum(lines.mapped('product_uom_qty')) or 1.0
+            extra_per_unit = order.total_extra / total_qty
+            for line in lines:
+                base = line.x_base_price if line.x_base_price > 0 else line.product_id.lst_price
+                line.price_unit = base + extra_per_unit
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    x_is_charge_line = fields.Boolean(string="Is Charge Line", default=False)
+    x_base_price = fields.Float(string="Base Price", digits='Product Price')
+
+    @api.onchange('product_id')
+    def _onchange_product_id_set_base(self):
+        if self.product_id:
+            self.x_base_price = self.product_id.lst_price
+
+    @api.onchange('x_base_price', 'product_uom_qty')
+    def _onchange_recompute_final_price(self):
+        if self.order_id:
+            self.order_id._recalculate_line_prices()
+
+    def _prepare_base_line_for_taxes_computation(self):
+        """
+        Odoo 17/18/19 Hook: Intercepts the tax engine.
+        Forces the tax engine to use x_base_price for its math,
+        ignoring the internal charges added to price_unit.
+        """
+        res = super()._prepare_base_line_for_taxes_computation()
+        if self.x_base_price > 0:
+            # We tell the tax engine that the price for TAX math is x_base_price
+            res['price_unit'] = self.x_base_price
+        return res
