c6d2e728485bfe5d4b33d2878dd5f16f69cd02c1
Vidhya <ebsvidyapl@gmail.com>
2026-05-27 15:14:37 +0530

    rever rfq drat code

--- custom/addons/purchase_custom_sequence/models/purchase_order.py
+++ custom/addons/purchase_custom_sequence/models/purchase_order.py
@@ -5,23 +5,24 @@ class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
 
     is_direct_po = fields.Boolean(
-        string='Direct Purchase Order',
-        help='If enabled, PO sequence is generated directly.'
+        string='Direct Purchase Order'
     )
+
     original_rfq_name = fields.Char(
         string='Original RFQ Number',
         copy=False
     )
+
     @api.model_create_multi
     def create(self, vals_list):
 
         for vals in vals_list:
 
-            # Skip if name already exists
+            # Skip if already has name
             if vals.get('name', '/') != '/':
                 continue
 
-            # Direct Purchase Order
+            # Direct PO
             if vals.get('is_direct_po'):
 
                 vals['name'] = self.env['ir.sequence'].next_by_code(
@@ -32,8 +33,8 @@ class PurchaseOrder(models.Model):
             else:
 
                 rfq_number = self.env['ir.sequence'].next_by_code(
-                        'purchase.order.rfq'
-                    ) or '/'
+                    'purchase.order.rfq'
+                ) or '/'
 
                 vals['name'] = rfq_number
                 vals['original_rfq_name'] = rfq_number
@@ -42,37 +43,37 @@ class PurchaseOrder(models.Model):
 
     def button_confirm(self):
 
-        # Call original confirmation
         res = super().button_confirm()
 
         for order in self:
 
-            # Only convert RFQ to PO
+            # Only RFQ conversion
             if not order.is_direct_po:
 
                 # Prevent duplicate conversion
                 if 'RFQ' in order.name:
 
-                    # Convert:
-                    # IP/RFQ/2025-26/004
-                    # to
-                    # IP/PO/2025-26/004
+                    # Extract running number
+                    sequence_number = order.name.split('/')[-1]
 
-                    order.name = order.name.replace(
-                        '/RFQ/',
-                        '/PO/'
-                    )
+                    fiscal_year = order.name.split('/')[2]
+
+                    # Create PO number using same running number
+                    po_number = f"IP/PO/{fiscal_year}/{sequence_number}"
+
+                    order.name = po_number
 
         return res
+
     def button_draft(self):
 
         res = super().button_draft()
 
         for order in self:
 
-        # Restore RFQ number
-         if not order.is_direct_po and order.original_rfq_name:
+            # Restore original RFQ number
+            if not order.is_direct_po and order.original_rfq_name:
 
-            order.name = order.original_rfq_name
+                order.name = order.original_rfq_name
 
         return res
