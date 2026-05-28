a10acb49943b413bd4bd025bd41baa4db6d78260
Vidhya <ebsvidyapl@gmail.com>
2026-05-28 15:12:15 +0530

    invoice report

---
+++ custom/addons/custom_invoice_report/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Custom Invoice Report",
+    "version": "19.0.1.0.0",
+    "category": "Accounting",
+    "summary": "Custom Invoice PDF Report",
+    "depends": ["account"],
+    "author": "Embark",
+    "license": "LGPL-3",
+    "data": [
+        "report/invoice_report.xml",
+        "report/invoice_template.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
