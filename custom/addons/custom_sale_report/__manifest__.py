f41372f9b5cfaedd47611266aa759cd96401ce61
Vidhya <ebsvidyapl@gmail.com>
2026-05-28 10:36:14 +0530

    add sales report 1

--- custom/addons/custom_sale_report/__manifest__.py
+++ custom/addons/custom_sale_report/__manifest__.py
@@ -1,14 +1,17 @@
 {
     'name': 'Custom Sale Order Report',
-    'version': '19.0.1.0',
+    'version': '17.0.1.0',
     'category': 'Sales',
     'summary': 'Custom Sales Order PDF Report',
     'depends': ['sale'],
     'author': 'Embark',
     'license': 'LGPL-3',
+
     'data': [
         'reports/sale_report.xml',
+        'reports/sale_order_template.xml',
     ],
+
     'installable': True,
     'application': False,
 }
