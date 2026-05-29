7199eaf6cfd7c97049e83ff3b2773dffe6270a53
Vidhya <ebsvidyapl@gmail.com>
2026-05-29 12:42:08 +0530

    fix studio bug

--- custom/addons/sale_dual_sequence/__manifest__.py
+++ custom/addons/sale_dual_sequence/__manifest__.py
@@ -5,7 +5,6 @@
     "depends": ["sale_management", "stock", "account"],
     "data": [
         "data/sale_sequence.xml",
-        "views/sale_order_views.xml",
         "reports/sale_report.xml",
     ],
     "installable": True,
