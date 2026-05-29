575c54338ea1038db97263e20091068dc4d5b970
Vidhya <ebsvidyapl@gmail.com>
2026-05-29 21:46:23 +0530

    Deliverslip folder

---
+++ custom/addons/custom_delivery_slip_report/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Custom Delivery Slip Report",
+    "version": "19.0.1.0.0",
+    "category": "Inventory",
+    "summary": "Custom Delivery Slip PDF Report",
+    "depends": ["stock"],
+    "author": "Embark",
+    "license": "LGPL-3",
+    "data": [
+        "report/delivery_slip_report.xml",
+        "report/delivery_slip_template.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
