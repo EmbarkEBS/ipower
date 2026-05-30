66d4d3f0d6ce9b733357e99c5b0926e5fbd4a67f
Vidhya <ebsvidyapl@gmail.com>
2026-05-30 13:07:31 +0530

    invoice company bank

---
+++ custom/addons/invoice_company_bank/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Invoice Company Bank Selection',
+    'version': '1.0',
+    'license': 'LGPL-3',
+    'depends': ['account'],
+    'data': [
+        'views/res_partner_bank_views.xml',
+        'views/account_move_views.xml',
+    ],
+    'installable': True,
+}
---
+++ custom/addons/invoice_company_bank/models/res_partner_bank.py
@@ -0,0 +1,9 @@
+from odoo import models, fields
+
+
+class ResPartnerBank(models.Model):
+    _inherit = "res.partner.bank"
+
+    iban = fields.Char(
+        string="IBAN Number"
+    )
