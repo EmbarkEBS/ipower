b1251c2b6fce524d4aacb8b8766d4adc587103d4
Vidhya <ebsvidyapl@gmail.com>
2026-06-03 12:29:31 +0530

    proforma report added

---
+++ custom/addons/custom_proforma_report/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Custom Proforma Invoice Report",
+    "version": "19.0.1.0.0",
+    "category": "Accounting",
+    "summary": "Custom Proforma Invoice PDF Report",
+    "depends": ["account"],
+    "author": "Embark",
+    "license": "LGPL-3",
+    "data": [
+        "report/invoice_report.xml",
+        "report/proforma_template.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
