05ac84a1db43611eb7b3015fc742080ae803bf81
Vidhya <ebsvidyapl@gmail.com>
2026-06-04 23:27:27 +0530

    hide print option

--- custom/addons/custom_sale_report/models/sale_order.py
+++ custom/addons/custom_sale_report/models/sale_order.py
@@ -4,10 +4,6 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     @api.onchange('partner_id')
-    def _onchange_partner_id(self):
-        res = super()._onchange_partner_id()
-
-        if self.partner_id and self.partner_id.x_studio_incoterm:
+    def _onchange_partner_incoterm(self):
+        if self.partner_id.x_studio_incoterm:
             self.incoterm = self.partner_id.x_studio_incoterm.id
-
-        return res
