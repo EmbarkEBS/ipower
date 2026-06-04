55b46c60f4c92d1c28853ae2f5734803dad0ca4a
Vidhya <ebsvidyapl@gmail.com>
2026-06-04 14:13:59 +0530

    payment made

---
+++ custom/addons/custom_payment_receipt/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    "name": "Custom Payment Receipt",
+    "version": "19.0.1.0.0",
+    "category": "Accounting",
+    "depends": ["account"],
+    "data": ["views/payment_receipt_report.xml"],
+    "installable": True,
+    "application": False,
+}
