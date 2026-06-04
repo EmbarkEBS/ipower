390332d224401742bba486ebb17dbdfd3de8fa44
Vidhya <ebsvidyapl@gmail.com>
2026-06-04 23:16:06 +0530

    hide print option

--- custom/addons/custom_sale_report/models/sale_order.py
+++ custom/addons/custom_sale_report/models/sale_order.py
@@ -1,9 +1,13 @@
-from odoo import api, fields, models
+from odoo import api, models
 
-class PurchaseOrder(models.Model):
-    _inherit = 'purchase.order'
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
 
     @api.onchange('partner_id')
-    def _onchange_partner_incoterm(self):
+    def _onchange_partner_id(self):
+        res = super()._onchange_partner_id()
+
         if self.partner_id.x_studio_incoterm:
-            self.incoterm_id = self.partner_id.x_studio_incoterm.id
+            self.incoterm = self.partner_id.x_studio_incoterm
+
+        return res
