5296e3f3a80710d924d1c4f6009b9e478ded4458
Vidhya <ebsvidyapl@gmail.com>
2026-06-04 15:00:00 +0530

    payment made custom module

--- custom/addons/custom_payment_receipt/__manifest__.py
+++ custom/addons/custom_payment_receipt/__manifest__.py
@@ -3,7 +3,10 @@
     "version": "19.0.1.0.0",
     "category": "Accounting",
     "depends": ["account"],
-    "data": ["views/payment_receipt_layout.xml"],
+    'data': [
+    'views/external_layout_payment_receipt.xml',
+    'views/payment_receipt_layout.xml',
+],
     "installable": True,
     "application": False,
 }
