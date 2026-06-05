f0f70f8bb7712f31441d1cd9da0c93d16d9327c9
Vidhya <ebsvidyapl@gmail.com>
2026-06-05 11:22:06 +0530

    print button to so

--- custom/addons/custom_sale_report/models/sale_order.py
+++ custom/addons/custom_sale_report/models/sale_order.py
@@ -3,6 +3,11 @@ from odoo import api, models
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
+    def action_print_custom_po(self):
+        return self.env.ref(
+           "custom_sale_report.action_report_custom_sale_order"
+        ).report_action(self)
+    
     @api.onchange('partner_id')
     def _onchange_partner_incoterm(self):
         if self.partner_id.x_studio_incoterm:
