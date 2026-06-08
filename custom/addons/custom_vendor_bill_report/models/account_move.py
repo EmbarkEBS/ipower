77e0b42c92309565e13989857cf3c0128c01d8f8
Vidhya <ebsvidyapl@gmail.com>
2026-06-08 12:29:32 +0530

    add preview in vendor bill

---
+++ custom/addons/custom_vendor_bill_report/models/account_move.py
@@ -0,0 +1,20 @@
+from odoo import models
+
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    def action_preview_custom_vendor_bill(self):
+        self.ensure_one()
+
+        report = self.env.ref(
+            "custom_vendor_bill_report.action_custom_vendor_bill_report"
+        )
+
+        return {
+            "type": "ir.actions.act_url",
+            "url": "/report/html/%s/%s" % (
+                report.report_name,
+                self.id,
+            ),
+            "target": "new",
+        }
