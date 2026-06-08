c9255e3ba9f0c9b8df7c0109b0ea2c3ccf8bb018
Vidhya <ebsvidyapl@gmail.com>
2026-06-08 12:48:42 +0530

    add preview in vendor bill

--- custom/addons/custom_vendor_bill_report/models/account_move.py
+++ custom/addons/custom_vendor_bill_report/models/account_move.py
@@ -6,15 +6,8 @@ class AccountMove(models.Model):
     def action_preview_custom_vendor_bill(self):
         self.ensure_one()
 
-        report = self.env.ref(
-            "custom_vendor_bill_report.action_vendor_bill_report"
-        )
-
         return {
             "type": "ir.actions.act_url",
-            "url": "/report/html/%s/%s" % (
-                report.report_name,
-                self.id,
-            ),
+            "url": "/report/html/custom_vendor_bill_report.vendor_bill_template/%s" % self.id,
             "target": "new",
         }
