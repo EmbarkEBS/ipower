0f00e18662e4d0574818099d2af95919b4829d5e
Vidhya <ebsvidyapl@gmail.com>
2026-06-10 13:10:11 +0530

    Code change for po attention field

--- custom/addons/custom_po_report/models/purchase_order.py
+++ custom/addons/custom_po_report/models/purchase_order.py
@@ -2,7 +2,28 @@ from odoo import api, fields, models
 
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
+    
+    def action_print_custom_po(self):
+        return self.env.ref(
+            'custom_po_report.action_report_custom_purchase_order'
+        ).report_action(self)
 
+  
+    def action_preview_custom_po(self):
+        self.ensure_one()
+
+        report = self.env.ref(
+            "custom_po_report.action_report_custom_purchase_order"
+        )
+
+        return {
+            "type": "ir.actions.act_url",
+            "url": "/report/pdf/%s/%s" % (
+                report.report_name,
+                self.id,
+            ),
+            "target": "new",
+        }
     contact_person_id = fields.Many2one(
         'res.partner',
         string='Attention'
