142b41aaffabe380029f6257212e06f383e9f8cb
Vidhya <ebsvidyapl@gmail.com>
2026-06-10 14:08:47 +0530

    attention in so

--- custom/addons/custom_sale_report/models/sale_order.py
+++ custom/addons/custom_sale_report/models/sale_order.py
@@ -1,7 +1,12 @@
-from odoo import models, api
+from odoo import models, fields, api
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
+
+    contact_person_id = fields.Many2one(
+        'res.partner',
+        string='Attention'
+    )
 
     def action_print_custom_so(self):
         return self.env.ref(
@@ -9,9 +14,14 @@ class SaleOrder(models.Model):
         ).report_action(self)
 
     @api.onchange('partner_id')
-    def _onchange_partner_incoterm(self):
-        if self.partner_id.x_studio_incoterm:
+    def _onchange_partner_id_custom(self):
+
+        # Incoterm
+        if self.partner_id and self.partner_id.x_studio_incoterm:
             self.incoterm = self.partner_id.x_studio_incoterm.id
+
+        # Clear Attention when customer changes
+        self.contact_person_id = False
 
     def action_preview_custom_sale_order(self):
         self.ensure_one()
