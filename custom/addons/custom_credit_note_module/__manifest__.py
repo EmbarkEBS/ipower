420fa8d395ae7d7b5a537eaa14da801cce859901
Vidhya <ebsvidyapl@gmail.com>
2026-06-11 22:39:38 +0530

    add validity in so

--- custom/addons/custom_credit_note_module/__manifest__.py
+++
@@ -1,8 +0,0 @@
-{
-    "name": "Custom Credit Note Layout",
-    "version": "19.0.1.0.0",
-    "depends": ["account"],
-    "data": ["views/credit_note_layout.xml"],
-    "installable": True,
-    "application": False
-}
