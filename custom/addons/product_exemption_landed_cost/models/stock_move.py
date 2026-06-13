70e136ff56f461400c8ba0f00db90f2f17f1c1ef
EmbarkEBS <91586071+EmbarkEBS@users.noreply.github.com>
2026-06-13 11:33:54 +0530

    Update stock_move.py

--- custom/addons/product_exemption_landed_cost/models/stock_move.py
+++ custom/addons/product_exemption_landed_cost/models/stock_move.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class StockMove(models.Model):
@@ -9,8 +9,14 @@ class StockMove(models.Model):
             ('0', '0%'),
             ('5', '5%'),
         ],
-        string="Exemption",
-        related="product_id.product_tmpl_id.exemption_rate",
-        store=True,
-        readonly=False,
+        string="Exemption Rate",
+        copy=True,
     )
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            if vals.get('product_id') and not vals.get('exemption_rate'):
+                product = self.env['product.product'].browse(vals['product_id'])
+                vals['exemption_rate'] = product.product_tmpl_id.exemption_rate
+        return super().create(vals_list)
