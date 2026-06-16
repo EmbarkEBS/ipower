8fcd4ae110f949dbf8253f13808262f591f0dd0c
Vidhya <ebsvidyapl@gmail.com>
2026-06-16 12:39:34 +0530

    rfq report

---
+++ custom/addons/purchase_analysis_shipping_odoo19/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Purchase Analysis Shipping Fields",
+    "version": "19.0.1.0.0",
+    "category": "Purchases",
+    "depends": ["purchase"],
+    "data": [
+        "views/purchase_report_views.xml"
+    ],
+    "installable": True,
+    "application": false
+}
--- custom/addons/purchase_report_shipping_fields/__manifest__.py
+++
@@ -1,7 +0,0 @@
-{
-    'name': 'Purchase Report Shipping Fields',
-    'version': '17.0.1.0.0',
-    'depends': ['purchase'],
-    'data': [],
-    'installable': True,
-}
