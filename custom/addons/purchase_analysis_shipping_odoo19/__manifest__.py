78d3b2c4164642da019dbad48132b421a0c65ab2
Vidhya <ebsvidyapl@gmail.com>
2026-06-16 12:44:31 +0530

    rfq report

--- custom/addons/purchase_analysis_shipping_odoo19/__manifest__.py
+++ custom/addons/purchase_analysis_shipping_odoo19/__manifest__.py
@@ -1,10 +1,14 @@
 {
     "name": "Purchase Analysis Shipping Fields",
     "version": "19.0.1.0.0",
+    "category": "Purchase",
+    "summary": "Custom Purchase Order PDF Report",
     "depends": ["purchase"],
+    'author': 'Embark',
+    'license': 'LGPL-3',
     "data": [
         "views/purchase_report_views.xml"
     ],
     "installable": True,
-    "application": false
+    "application": False,
 }
