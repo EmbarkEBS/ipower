795f203a4598011c915b21dbf6f93e75b751cb40
Vidhya <ebsvidyapl@gmail.com>
2026-06-16 12:31:34 +0530

    rfq report

---
+++ custom/addons/purchase_report_shipping_fields/__manifest__.py
@@ -0,0 +1,7 @@
+{
+    'name': 'Purchase Report Shipping Fields',
+    'version': '17.0.1.0.0',
+    'depends': ['purchase'],
+    'data': [],
+    'installable': True,
+}
---
+++ custom/addons/purchase_report_shipping_fields/models/purchase_report.py
@@ -0,0 +1,20 @@
+
+from odoo import fields, models
+
+class PurchaseReport(models.Model):
+    _inherit = 'purchase.report'
+
+    x_studio_weightkg_1 = fields.Char(readonly=True)
+    x_studio_dimensions_1 = fields.Char(readonly=True)
+    x_studio_pickup_address = fields.Char(readonly=True)
+    x_studio_delivery_location = fields.Char(readonly=True)
+
+    def _select_additional_fields(self):
+        res = super()._select_additional_fields()
+        res.update({
+            'x_studio_weightkg_1': 'po.x_studio_weightkg_1',
+            'x_studio_dimensions_1': 'po.x_studio_dimensions_1',
+            'x_studio_pickup_address': 'po.x_studio_pickup_address',
+            'x_studio_delivery_location': 'po.x_studio_delivery_location',
+        })
+        return res
