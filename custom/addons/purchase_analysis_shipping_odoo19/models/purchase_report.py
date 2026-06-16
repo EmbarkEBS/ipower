bf2e1ab8ba7852e677cd25fd96cb6b692feca06f
Vidhya <ebsvidyapl@gmail.com>
2026-06-16 12:58:50 +0530

    rfq report 2

--- custom/addons/purchase_analysis_shipping_odoo19/models/purchase_report.py
+++ custom/addons/purchase_analysis_shipping_odoo19/models/purchase_report.py
@@ -1,19 +1,48 @@
+# -*- coding: utf-8 -*-
+
 from odoo import fields, models
+from odoo.tools.sql import SQL
+
 
 class PurchaseReport(models.Model):
-    _inherit = 'purchase.report'
+    _inherit = "purchase.report"
 
-    x_studio_weightkg_1 = fields.Char(readonly=True)
-    x_studio_dimensions_1 = fields.Char(readonly=True)
-    x_studio_pickup_address = fields.Char(readonly=True)
-    x_studio_delivery_location = fields.Char(readonly=True)
+    x_studio_weightkg_1 = fields.Char(
+        string="Weight (KG)",
+        readonly=True,
+    )
 
-    def _select_additional_fields(self):
-        res = super()._select_additional_fields()
-        res.update({
-            'x_studio_weightkg_1': 'po.x_studio_weightkg_1',
-            'x_studio_dimensions_1': 'po.x_studio_dimensions_1',
-            'x_studio_pickup_address': 'po.x_studio_pickup_address',
-            'x_studio_delivery_location': 'po.x_studio_delivery_location',
-        })
-        return res
+    x_studio_dimensions_1 = fields.Char(
+        string="Dimensions",
+        readonly=True,
+    )
+
+    x_studio_pickup_address = fields.Char(
+        string="Pickup Address",
+        readonly=True,
+    )
+
+    x_studio_delivery_location = fields.Char(
+        string="Delivery Location",
+        readonly=True,
+    )
+
+    def _select(self):
+        return SQL(
+            "%s, "
+            "po.x_studio_weightkg_1 AS x_studio_weightkg_1, "
+            "po.x_studio_dimensions_1 AS x_studio_dimensions_1, "
+            "po.x_studio_pickup_address AS x_studio_pickup_address, "
+            "po.x_studio_delivery_location AS x_studio_delivery_location",
+            super()._select(),
+        )
+
+    def _group_by(self):
+        return SQL(
+            "%s, "
+            "po.x_studio_weightkg_1, "
+            "po.x_studio_dimensions_1, "
+            "po.x_studio_pickup_address, "
+            "po.x_studio_delivery_location",
+            super()._group_by(),
+        )
