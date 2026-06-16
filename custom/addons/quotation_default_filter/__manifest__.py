60aa61fa5b08d4c77cccf9b3f67751411c2b1282
EmbarkEBS <91586071+EmbarkEBS@users.noreply.github.com>
2026-06-16 11:16:10 +0530

    Update __manifest__.py

--- custom/addons/quotation_default_filter/__manifest__.py
+++ custom/addons/quotation_default_filter/__manifest__.py
@@ -2,7 +2,6 @@
     "name": "Quotation Default Filter",
     "version": "19.0.1.0.0",
     "category": "Sales",
-    "summary": "Show only My Quotations in Draft and Sent state",
     "depends": ["sale"],
     "data": [
         "views/sale_action.xml",
