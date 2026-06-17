7b6f95376e5d2b5695a747f68f3951b2965b00d0
EmbarkEBS <91586071+EmbarkEBS@users.noreply.github.com>
2026-06-17 16:31:47 +0530

    Update account_move.py

--- custom/addons/invoice_company_bank/models/account_move.py
+++ custom/addons/invoice_company_bank/models/account_move.py
@@ -1,3 +1,51 @@
+# from odoo import models, fields, api
+
+
+# class AccountMove(models.Model):
+#     _inherit = "account.move"
+
+#     company_bank_id = fields.Many2one(
+#         "res.partner.bank",
+#         string="Company Bank Account",
+#         copy=False,
+#     )
+
+#     bank_display_details = fields.Html(
+#         string="Bank Details",
+#         compute="_compute_bank_display_details",
+#     )
+
+#     @api.onchange("company_id")
+#     def _onchange_company_id_bank(self):
+#         return {
+#             "domain": {
+#                 "company_bank_id": [
+#                     ("partner_id", "=", self.company_id.partner_id.id)
+#                 ]
+#             }
+#         }
+
+#     @api.depends("company_bank_id")
+#     def _compute_bank_display_details(self):
+#         for move in self:
+#             bank = move.company_bank_id
+
+#             if not bank:
+#                 move.bank_display_details = False
+#                 continue
+
+#             move.bank_display_details = """
+#                 <b>Bank Name:</b> %s<br/>
+#                 <b>Account Number:</b> %s<br/>
+#                 <b>IBAN Number:</b> %s<br/>
+#                 <b>Account Holder:</b> %s
+#             """ % (
+#                 bank.bank_id.name or "",
+#                 bank.acc_number or "",
+#                 bank.iban or "",
+#                 bank.partner_id.name or "",
+#             )
+
 from odoo import models, fields, api
 
 
@@ -7,6 +55,7 @@ class AccountMove(models.Model):
     company_bank_id = fields.Many2one(
         "res.partner.bank",
         string="Company Bank Account",
+        domain="[('partner_id', '=', company_id.partner_id)]",
         copy=False,
     )
 
@@ -14,16 +63,6 @@ class AccountMove(models.Model):
         string="Bank Details",
         compute="_compute_bank_display_details",
     )
-
-    @api.onchange("company_id")
-    def _onchange_company_id_bank(self):
-        return {
-            "domain": {
-                "company_bank_id": [
-                    ("partner_id", "=", self.company_id.partner_id.id)
-                ]
-            }
-        }
 
     @api.depends("company_bank_id")
     def _compute_bank_display_details(self):
