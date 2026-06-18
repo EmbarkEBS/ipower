5031ead82646245beb3bdbed8ee04fb32acb9c22
Vidhya <ebsvidyapl@gmail.com>
2026-06-18 09:54:16 +0530

    staging to production

---
+++ custom/addons/expense_hide_category/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Expense Hide Category",
+    "version": "19.0.1.0.0",
+    "category": "Human Resources",
+    "summary": "Hide expense category and set default category",
+    "depends": ["hr_expense"],
+    "data": [
+        "views/hr_expense_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
---
+++ custom/addons/expense_hide_category/models/hr_expense.py
@@ -0,0 +1,20 @@
+from odoo import models, api
+
+
+class HrExpense(models.Model):
+    _inherit = "hr.expense"
+
+    @api.model
+    def default_get(self, fields_list):
+        res = super().default_get(fields_list)
+
+        if not res.get("product_id"):
+            product = self.env["product.product"].search(
+                [('can_be_expensed', '=', True)],
+                limit=1
+            )
+            if product:
+                res["product_id"] = product.id
+                res['name'] = ''
+
+        return res
