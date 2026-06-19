a4299ad0291069dcafb0b0b7d9c7e146b080c083
EmbarkEBS <91586071+EmbarkEBS@users.noreply.github.com>
2026-06-19 11:20:08 +0530

    Customer Followup report logo

---
+++ custom/addons/account_report_logo/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Account Report Logo",
+    "version": "19.0.1.0.0",
+    "category": "Accounting",
+    "depends": [
+        "account",
+        "account_followup",
+    ],
+    "data": [
+        "views/report_logo.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
