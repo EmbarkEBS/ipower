6f8aea1e344088ff50c07a5bf1cebf6c91b50dda
Vidhya <ebsvidyapl@gmail.com>
2026-06-19 13:19:16 +0530

    invoice disable

--- custom/addons/disable_invoice_attachment/models/account_move_send.py
+++ custom/addons/disable_invoice_attachment/models/account_move_send.py
@@ -1,8 +1,11 @@
+import logging
 from odoo import models, api
 
-class AccountMoveSendWizard(models.TransientModel):
-    _inherit = 'account.move.send.wizard'
+_logger = logging.getLogger(__name__)
 
+
+class AccountMoveSendWizard(models.TransientModel):
+    _inherit = "account.move.send.wizard"
 
     @api.model
     def _get_default_mail_attachments_widget(self, moves, *args, **kwargs):
@@ -10,15 +13,9 @@ class AccountMoveSendWizard(models.TransientModel):
             moves, *args, **kwargs
         )
 
-        filtered_attachments = []
+        _logger.warning("=== ATTACHMENTS START ===")
+        for att in attachments:
+            _logger.warning("%s", att)
+        _logger.warning("=== ATTACHMENTS END ===")
 
-        for attachment in attachments:
-            name = attachment.get("name", "")
-
-            # Remove standard invoice PDF
-            if name.endswith(".pdf") and not name.startswith("Invoice-"):
-                continue
-
-            filtered_attachments.append(attachment)
-
-        return filtered_attachments
+        return attachments
