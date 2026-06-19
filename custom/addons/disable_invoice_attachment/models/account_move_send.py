0d8866dfc86855a018a8fa04d0f71a8e216b4db3
Vidhya <ebsvidyapl@gmail.com>
2026-06-19 13:35:59 +0530

    invoice disable

--- custom/addons/disable_invoice_attachment/models/account_move_send.py
+++ custom/addons/disable_invoice_attachment/models/account_move_send.py
@@ -1,21 +1,19 @@
-import logging
 from odoo import models, api
-
-_logger = logging.getLogger(__name__)
-
 
 class AccountMoveSendWizard(models.TransientModel):
     _inherit = "account.move.send.wizard"
 
+
     @api.model
     def _get_default_mail_attachments_widget(self, moves, *args, **kwargs):
-        attachments = super()._get_default_mail_attachments_widget(
-            moves, *args, **kwargs
-        )
+            attachments = super()._get_default_mail_attachments_widget(
+                moves, *args, **kwargs
+            )
 
-        _logger.warning("=== ATTACHMENTS START ===")
-        for att in attachments:
-            _logger.warning("%s", att)
-        _logger.warning("=== ATTACHMENTS END ===")
+            # Keep only dynamic reports (your custom report)
+            return [
+                att
+                for att in attachments
+                if att.get("dynamic_report")
+            ]
 
-        return attachments
