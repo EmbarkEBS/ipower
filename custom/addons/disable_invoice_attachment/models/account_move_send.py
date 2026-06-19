203a5147fc4a3b2b44207ace1544a37178a04350
Vidhya <ebsvidyapl@gmail.com>
2026-06-19 13:43:53 +0530

    invoice disable

--- custom/addons/disable_invoice_attachment/models/account_move_send.py
+++ custom/addons/disable_invoice_attachment/models/account_move_send.py
@@ -6,14 +6,29 @@ class AccountMoveSendWizard(models.TransientModel):
 
     @api.model
     def _get_default_mail_attachments_widget(self, moves, *args, **kwargs):
-            attachments = super()._get_default_mail_attachments_widget(
-                moves, *args, **kwargs
-            )
+        attachments = super()._get_default_mail_attachments_widget(
+            moves, *args, **kwargs
+        )
 
-            # Keep only dynamic reports (your custom report)
-            return [
-                att
-                for att in attachments
-                if att.get("dynamic_report")
-            ]
+        return [
+            att for att in attachments
+            if att.get("dynamic_report")
+        ]
+
+    def _get_mail_params(self, move, mail_template, **kwargs):
+        params = super()._get_mail_params(
+            move, mail_template, **kwargs
+        )
+
+        attachment_ids = params.get("attachment_ids", [])
+
+        attachments = self.env["ir.attachment"].browse(attachment_ids)
+
+        keep_attachments = attachments.filtered(
+            lambda a: a.name.startswith("Invoice-")
+        )
+
+        params["attachment_ids"] = keep_attachments.ids
+
+        return params
 
