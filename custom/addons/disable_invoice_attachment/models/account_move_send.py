2571e52667d23f8d2fff4826f492c14a90607e04
Vidhya <ebsvidyapl@gmail.com>
2026-06-19 22:53:07 +0530

    replace defalt invoice to custom repot in email

--- custom/addons/disable_invoice_attachment/models/account_move_send.py
+++ custom/addons/disable_invoice_attachment/models/account_move_send.py
@@ -1,12 +1,11 @@
-import logging
 from odoo import models
 
-_logger = logging.getLogger(__name__)
-
 class AccountMoveSend(models.AbstractModel):
-    _inherit = "account.move.send"
+ _inherit = "account.move.send"
 
-    def __getattribute__(self, name):
-        if "pdf" in name.lower() or "report" in name.lower():
-            _logger.warning("METHOD AVAILABLE: %s", name)
-        return super().__getattribute__(name)
+def _get_default_pdf_report_id(self, move):
+    return self.env.ref(
+        "custom_invoice_report.invoice_template",
+        raise_if_not_found=False,
+    )
+
