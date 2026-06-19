2d78c53de2e30d442eae68794bae56c291423cdd
EmbarkEBS <91586071+EmbarkEBS@users.noreply.github.com>
2026-06-19 18:46:52 +0530

    Update account_move.py

--- custom/addons/stock_boe_management/models/account_move.py
+++ custom/addons/stock_boe_management/models/account_move.py
@@ -6,36 +6,39 @@ class AccountMove(models.Model):
 
     boe_number = fields.Char(
         string="BOE Number",
+        compute="_compute_boe_fields",
         readonly=True,
-        copy=False,
+        store=True,
     )
 
     boe_date = fields.Date(
         string="BOE Date",
+        compute="_compute_boe_fields",
         readonly=True,
-        copy=False,
+        store=True,
     )
 
-    @api.model_create_multi
-    def create(self, vals_list):
-        moves = super().create(vals_list)
+    @api.depends(
+        "invoice_line_ids.purchase_line_id.order_id"
+    )
+    def _compute_boe_fields(self):
+        for move in self:
+            move.boe_number = False
+            move.boe_date = False
 
-        for move in moves:
-            if move.move_type != "in_invoice":
+            purchase_orders = move.invoice_line_ids.mapped(
+                "purchase_line_id.order_id"
+            )
+
+            if not purchase_orders:
                 continue
 
-            if not move.invoice_origin:
-                continue
-
-            picking = self.env["stock.picking"].search([
-                ("name", "=", move.invoice_origin),
+            receipt = self.env["stock.picking"].search([
+                ("purchase_id", "in", purchase_orders.ids),
                 ("picking_type_id.code", "=", "incoming"),
-            ], limit=1)
+                ("state", "=", "done"),
+            ], order="id desc", limit=1)
 
-            if picking:
-                move.sudo().write({
-                    "boe_number": picking.boe_number,
-                    "boe_date": picking.boe_date,
-                })
-
-        return moves
+            if receipt:
+                move.boe_number = receipt.boe_number
+                move.boe_date = receipt.boe_date
