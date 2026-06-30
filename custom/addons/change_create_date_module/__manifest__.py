b5720fa01aa47d52ee317c79c53a34d2928da9ab
Vidhya <ebsvidyapl@gmail.com>
2026-06-30 16:25:42 +0530

    SO creation date overide

---
+++ custom/addons/change_create_date_module/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Sale Order Create Date Editor',
+    'version': '19.0.1.0.0',
+    'depends': ['sale'],
+     'author': 'Embark',
+    'license': 'LGPL-3',
+'data': [
+        'views/sale_order_views.xml',
+    ],
+    'installable': True,
+}
