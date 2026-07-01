457b80c7b78bd08f696a3bf80c25306bbfe92fb6
Vidhya <ebsvidyapl@gmail.com>
2026-07-01 13:23:00 +0530

    override creration date po so add module

---
+++ custom/addons/purchase_create_date_editor/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Purchase RFQ Creation Date Editor',
+    'version': '19.0.1.0.0',
+    'depends': ['purchase'],
+    'data': [
+        'views/purchase_order_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
