c2c98dd0bcb43f8a586dec17c02f74a7f9d07bf5
Vidhya <ebsvidyapl@gmail.com>
2026-07-01 15:23:52 +0530

    sales and purchase dual sequence change

--- custom/addons/purchase_dual_sequence/models/purchase_order.py
+++ custom/addons/purchase_dual_sequence/models/purchase_order.py
@@ -1,3 +1,32 @@
+# from odoo import api, fields, models
+
+
+# class PurchaseOrder(models.Model):
+#     _inherit = 'purchase.order'
+
+#     is_rfq = fields.Boolean(default=True)
+
+#     @api.model_create_multi
+#     def create(self, vals_list):
+
+#         for vals in vals_list:
+#             if vals.get('name', 'New') == 'New':
+#                 vals['name'] = self.env['ir.sequence'].next_by_code(
+#                     'purchase.rfq'
+#                 ) or 'New'
+
+#         return super().create(vals_list)
+
+#     def button_confirm(self):
+
+#         for order in self:
+#             if order.is_rfq:
+#                 order.name = self.env['ir.sequence'].next_by_code(
+#                     'purchase.order.custom'
+#                 )
+#                 order.is_rfq = False
+
+#         return super().button_confirm()
 from odoo import api, fields, models
 
 
@@ -10,7 +39,15 @@ class PurchaseOrder(models.Model):
     def create(self, vals_list):
 
         for vals in vals_list:
-            if vals.get('name', 'New') == 'New':
+
+            # Intercompany auto-generated PO
+            if vals.get('auto_generated'):
+                vals['name'] = self.env['ir.sequence'].next_by_code(
+                    'purchase.rfq'
+                ) or 'New'
+
+            # Manual RFQ
+            elif vals.get('name', 'New') == 'New':
                 vals['name'] = self.env['ir.sequence'].next_by_code(
                     'purchase.rfq'
                 ) or 'New'
@@ -21,9 +58,11 @@ class PurchaseOrder(models.Model):
 
         for order in self:
             if order.is_rfq:
+
                 order.name = self.env['ir.sequence'].next_by_code(
                     'purchase.order.custom'
                 )
+
                 order.is_rfq = False
 
         return super().button_confirm()
