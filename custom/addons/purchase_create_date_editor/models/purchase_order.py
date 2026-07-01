b43eab2b19744bc3610436ff6c6ee7ee99606edf
Vidhya <ebsvidyapl@gmail.com>
2026-07-01 10:42:47 +0530

    override creration date po add module

---
+++ custom/addons/purchase_create_date_editor/models/purchase_order.py
@@ -0,0 +1,47 @@
+from odoo import models, fields
+
+
+class PurchaseOrder(models.Model):
+    _inherit = 'purchase.order'
+
+    # Creation Date
+    editable_create_date = fields.Datetime(
+        string="Creation Date",
+        compute="_compute_editable_dates",
+        inverse="_inverse_create_date",
+    )
+
+    # Confirmation Date
+    editable_confirmation_date = fields.Datetime(
+        string="Confirmation Date",
+        compute="_compute_editable_dates",
+        inverse="_inverse_confirmation_date",
+    )
+
+    def _compute_editable_dates(self):
+        for rec in self:
+            rec.editable_create_date = rec.create_date
+            rec.editable_confirmation_date = rec.date_approve
+
+    def _inverse_create_date(self):
+        for rec in self:
+            if rec.editable_create_date:
+                self.env.cr.execute("""
+                    UPDATE purchase_order
+                    SET create_date = %s
+                    WHERE id = %s
+                """, (
+                    rec.editable_create_date,
+                    rec.id,
+                ))
+
+    def _inverse_confirmation_date(self):
+        for rec in self:
+            self.env.cr.execute("""
+                UPDATE purchase_order
+                SET date_approve = %s
+                WHERE id = %s
+            """, (
+                rec.editable_confirmation_date,
+                rec.id,
+            ))
