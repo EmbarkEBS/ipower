5a64a7c1e2be1a752211d7deaff9cf129e26315a
EmbarkEBS <91586071+EmbarkEBS@users.noreply.github.com>
2026-08-04 11:24:14 +0530

    COA Duplication custom module

---
+++ custom/addons/account_duplicate_name_check/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Account Duplicate Name Prevention",
+    "version": "19.0.1.0.0",
+    "category": "Accounting",
+    "summary": "Prevent duplicate Chart of Account names",
+    "depends": ["account"],
+    "data": [],
+    "installable": True,
+    "application": False,
+    "license": "LGPL-3",
+}
