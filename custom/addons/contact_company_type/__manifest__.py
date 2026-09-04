78af2ed2ba1d87e97a0850b589357e9bf242ca9b
EmbarkEBS <91586071+EmbarkEBS@users.noreply.github.com>
2026-09-04 13:47:31 +0530

    Update __manifest__.py

--- custom/addons/contact_company_type/__manifest__.py
+++ custom/addons/contact_company_type/__manifest__.py
@@ -1,8 +1,7 @@
 {
-    'name': 'Contact Company Type - One Time',
+    'name': 'Contact One Time',
     'version': '19.0.1.0.0',
     'category': 'Contacts',
-    'summary': 'Adds One Time option to Contact Company Type',
     'depends': ['contacts'],
     'data': [
         'views/res_partner_views.xml',
