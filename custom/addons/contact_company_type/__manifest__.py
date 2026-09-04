20896f59a4ec07e48219ab4699f7b49415518e44
EmbarkEBS <91586071+EmbarkEBS@users.noreply.github.com>
2026-09-04 13:33:20 +0530

    Custom contact company type created

---
+++ custom/addons/contact_company_type/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Contact Company Type - One Time',
+    'version': '19.0.1.0.0',
+    'category': 'Contacts',
+    'summary': 'Adds One Time option to Contact Company Type',
+    'depends': ['contacts'],
+    'data': [
+        'views/res_partner_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
---
+++ custom/addons/contact_company_type/models/res_partner.py
@@ -0,0 +1,14 @@
+from odoo import models, fields
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    company_type = fields.Selection(
+        selection_add=[
+            ('one_time', 'One Time'),
+        ],
+        ondelete={
+            'one_time': 'set default',
+        },
+    )
